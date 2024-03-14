156a567ca67ecdfbd17dc6350768a1bddfcca610
VilgotKH <vilgot.wahlgren@gmail.com>
2024-03-14 18:07:24 +0000

    added css

--- Makefile
+++ Makefile
@@ -1,3 +1,4 @@
 make:
 	sudo cp client.html /var/www/html/index.html
 	sudo cp client.js /var/www/html/client.js
+	sudo cp client.css /var/www/html/client.css
--- client.js
+++ client.js
@@ -2,34 +2,58 @@
 	const server = "http://10.1.3.151";
 	const port = 8080;
         const xhttp = new XMLHttpRequest();
+	var name = prompt("Enter name:");
+	var curChannel;
+	var avalibleChannels = [];
+	var members = [];
+	function channelButton(id) {
+		curChannel = id;
+		get("load");
+		document.getElementById("curchannel").innerHTML = "Current channel: " + curChannel;
+
+	}
+	var channelUpdate = window.setInterval(function() {
+		get("channels");
+		document.getElementById("sidebar").innerHTML = "";
+		for (i = 0; i < avalibleChannels.channel.length; i++) {
+			ChlId = avalibleChannels.channel[i].text;
+			document.getElementById("sidebar").innerHTML += "<button id='" + ChlId + "' onclick='channelButton(" + ChlId + ");'>" + ChlId + "</button><br>";
+		}
+		get("load");
+	},1000);
         function get(method) {
-            xhttp.open("GET", server + ":" + port + "?" + method + "&" + document.getElementById("chan").value + "&" + document.getElementById("name").value, false)
+            xhttp.open("GET", server + ":" + port + "?" + method + "&" + curChannel + "&" + name, false)
             xhttp.onload = () => {
                 resp = JSON.parse(xhttp.response);
                 can = "";
+		if (method == "channels") {
+			avalibleChannels = resp;
+			return;
+		}
                 for(i = 0; i < resp.channel.length; i++) {
                     can += resp.channel[i].user + ": " + resp.channel[i].text + "<br>";
                 }
-                document.getElementById('value').innerHTML = can + resp.members;
-            }
+                document.getElementById('value').innerHTML = can;
+		members = resp.members;
+	    }
             xhttp.send("hallo")
             //setTimeout(get, 1000)
         }
         function send() {
             xhttp.open("POST", server + ":" + port, true)
-            msg = {method: "sendMsg", data: {user: document.getElementById("name").value, text: document.getElementById("input").value, channel: document.getElementById("chan").value}}
+            msg = {method: "sendMsg", data: {user: name, text: document.getElementById("input").value, channel: curChannel}}
             xhttp.send(JSON.stringify(msg))
             get("load");
         }
 	function make() {
 	    xhttp.open("POST", server + ":" + port, true)
-            msg = {method: "makeCnl", data: {user: document.getElementById("name").value}}
+            msg = {method: "makeCnl", data: {user: name}}
             console.log(xhttp.send(JSON.stringify(msg)));
 	    get("channels");
 	}
 	function add() {
 	    xhttp.open("POST", server + ":" + port, true)
-            msg = {method: "addUsr", data: {user: document.getElementById("name").value, channel: document.getElementById("chan").value, addUser: document.getElementById("adusr").value}}
+            msg = {method: "addUsr", data: {user: name, channel: curChannel, addUser: document.getElementById("adusr").value}}
             console.log(xhttp.send(JSON.stringify(msg)));
 	
 	}
